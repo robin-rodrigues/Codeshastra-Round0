194c1894a9ba409cf590ff85314a47c78ea664ce
Rushikesh/1999 <rushi.shinde1999@gmail.com>
2020-01-25 16:13:37 +0530

    Added ngo model

---
+++ models/ngo.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ngoSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String
+  },
+  sectors: {
+      type: String
+  }
+  
+});
+
+module.exports = mongoose.model('Ngo', ngoSchema);
